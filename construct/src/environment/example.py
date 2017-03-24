d7957bd618274f44042f0bb0e588767f9b989060
robert.pottorff <rpottorff@gmail.com>
2017-03-24 15:13:34 -0600

    added est trajectory to example

--- construct/src/environment/example.py
+++ construct/src/environment/example.py
@@ -2,14 +2,22 @@ from construct import GazeboEnvironment
 from tqdm import tqdm
 import numpy as np
 import math
-
+import tf.transformations as tft
 
 g = GazeboEnvironment(verbose=True)
 
 for e in tqdm(range(10000)):
     state = g.reset()
     for i in (range(1000)):
-        action = [5, 0, 0, -10.0]
+
+        gx, gy, gz = g.world_manager.world.goal
+        cx, cy, cz = state[0][:3]
+        qx, qy, qz, qw = state[0][3:7]
+        r, p, cyaw = tft.euler_from_quaternion(state[0][3:7])
+        gyaw = math.atan2(gy - cy, gx - cx)
+
+        action = np.clip([.5 * (gy - cy) * math.sin(cyaw), .5 * (gx - cx) * math.cos(cyaw), gyaw - cyaw, -10.0], -10, 10)
+
         state, reward, terminal, _ = g.step(action)
 
         if terminal:
